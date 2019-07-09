56a6a15b573b0f64ff3d806c9f1f9e4f1f61de94
NGribkov <52722671+NGribkov@users.noreply.github.com>
2019-07-10 01:33:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class PpTest(unittest.TestCase):
 		self.df1 = pd.read_csv('orders.csv')
 		self.assertEqual(len(self.df1['OrderId']), len(self.df1['OrderId'].unique()))
         
-    #check if resulting dataframe is not greater then unique ProductId's
+    	#check if resulting dataframe is not greater then unique ProductId's
 	def test_check_lenght(self):
 		self.df1 = pd.read_csv('orders.csv')
 		self.df2 = pd.read_csv('order_lines.csv')
